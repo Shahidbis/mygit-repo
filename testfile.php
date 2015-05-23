9bc2eecf40a12bfda6b0ef429c24e99ffe14c1c8
Shahidedy <shahid.mohammad@edynamic.net>
2015-05-23 13:11:42 +0530

    v2.0: modified index file print message

--- testfile.php
+++ testfile.php
@@ -2,4 +2,6 @@
 
 echo "this is a new file creation in testing branch";
 
+echo "i am adding this line to check the modification effect did in testing branch and then merging it with training branch";
+
 ?>
