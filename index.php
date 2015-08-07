8adf74e8e9919f275185844da738f8c71b6865b8
shahidedy <shahid.mohammad@edynamic.net>
2015-08-08 00:38:25 +0530

    revert mesg commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "hi i am here";
 echo "this is just to check if changes are done in a file in testing, it can be taken to traing, then stage and then master";
-
+echo "this is revert testing msg";
 phpinfo();
 
 ?>
