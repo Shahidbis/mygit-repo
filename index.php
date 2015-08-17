d26267564c1ce81242f833dd1fad4f6fefc6bff2
Shahidedy <shahid.mohammad@edynamic.net>
2015-08-17 17:16:10 +0530

    reset push

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ echo "hi i am here";
 echo "this is just to check if changes are done in a file in testing, it can be taken to traing, then stage and then master";
 echo "this is revert testing msg";
 echo "this is new line added via storm";
-echo "this is hii";
 phpinfo();
 
 ?>
