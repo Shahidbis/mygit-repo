3cef66865ee1ea4826c2f3daca6080704dbd33ea
Shahidedy <shahid.mohammad@edynamic.net>
2015-08-17 14:23:47 +0530

    updated msg on 17-aug

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 echo "hi i am here";
 echo "this is just to check if changes are done in a file in testing, it can be taken to traing, then stage and then master";
 echo "this is revert testing msg";
+echo "this is new line added via storm";
 phpinfo();
 
 ?>
