582113f0033efdf0b786cecad47b67f62a92a7a0
ShahidM <shahid.mohammad@edynamic.net>
2016-01-13 13:56:19 +0530

    Change in newfile only-2

--- newfileonserver.php
+++ newfileonserver.php
@@ -2,5 +2,5 @@
 echo "this is new file directly uploaded on the server and now this has to be pulled, worked, and then pushed on staging";
 echo 'fixed something';
 echo 'fixed something';
-
+echo 'another test';
 ?>
